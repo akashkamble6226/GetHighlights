83ab2aef56e8392f9f1e6b0d963648d8a019469f
akash <akashkamble6226@gmail.com>
2026-05-11 00:25:47 +0530

    fixed cors issue

--- src/backend/src/main.ts
+++ src/backend/src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      "http://localhost:5173",
+      "https://get-highlights-q4jv.vercel.app/",
+    ],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
